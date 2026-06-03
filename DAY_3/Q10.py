07b23977ed4b2217d12b871e5c182974007824ae
swasti rana <swastiran06@gmail.com>
2026-06-03 17:12:14 +0530

    DAY_3 assignment is uploaded

---
+++ DAY_3/Q10.py
@@ -0,0 +1,12 @@
+# Prime numbers in a range 
+
+start=int(input("Enter a starting number:"))
+end=int(input("Enter ending numbers:"))
+
+for num in range(start,end+1):
+    if num>1:
+        for i in range(2,num):
+            if num % i==0:
+                break
+        else:
+            print(num,end=" ")
---
+++ DAY_3/Q9.py
@@ -0,0 +1,13 @@
+# To check whether a number is prime 
+
+num=int(input("enter a number:"))
+
+if num <=1:
+    print("not prime")
+else:
+    for i in range (2,num):
+        if num % i==0:
+            print("not prime")
+            break
+    else:
+        print("prime")
