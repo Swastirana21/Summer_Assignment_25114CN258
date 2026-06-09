1c391afea4e917bc6afc93f24dc842b3d589846e
swasti rana <swastiran06@gmail.com>
2026-06-09 14:55:03 +0530

    DAY_9 assignment is uploaded

---
+++ DAY_9/Q33.py
@@ -0,0 +1,4 @@
+# To print reverse star oattern
+rows=5
+for i in range(rows,0,-1):
+    print("*"*i)
---
+++ DAY_9/Q35.py
@@ -0,0 +1,5 @@
+# To print repeated character pattern
+rows=5
+for i in range(5):
+    print(chr(65+i)*(i+1))
+    
