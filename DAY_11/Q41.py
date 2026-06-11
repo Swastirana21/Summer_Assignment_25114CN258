2033ed276d47beeb056dd1787183274087bfc6e4
swasti rana <swastiran06@gmail.com>
2026-06-11 19:11:05 +0530

    DAY_11 assignment is uploaded

---
+++ DAY_11/Q41.py
@@ -0,0 +1,7 @@
+# Function to find sum of two numbers
+
+def add(a,b):
+    return a+b
+
+print(add(10,20))
+    
---
+++ DAY_11/Q42.py
@@ -0,0 +1,6 @@
+# Function to find maximum
+
+def maximum(a,b):
+    return a if a>b else b
+
+print(maximum(100,201))
